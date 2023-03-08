e5dd7abdafb4e9b818b72f4d58f938936d36fdac
Rio Edwards <rioredwards@gmail.com>
2023-03-08 15:57:29 -0800

    Basics

--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+/* Object Types */
+// Generic object type
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Rio",
+  age: 25,
+};
+
+console.log(person.name);
