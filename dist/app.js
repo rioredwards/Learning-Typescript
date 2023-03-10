8d9e1efa6d25ffd1141e7639a95f8f27f8094f8a
Rio Edwards <rioredwards@gmail.com>
2023-03-09 17:14:54 -0800

    Static properties + methods

--- dist/app.js
+++ dist/app.js
@@ -7,6 +7,9 @@ class Department {
         // Protected: private but accessible in inherited classes
         this.employees = [];
     }
+    static createEmployee(name) {
+        return { name: name };
+    }
     describe() {
         console.log("Department: " + this.name + " (" + this.id + ")");
     }
@@ -18,45 +21,6 @@ class Department {
         console.log(this.employees);
     }
 }
-// Inheritance: This class automatically gets all the properties and methods of the Department class
-// and we can add more properties and methods to extend it
-class AccountingDepartment extends Department {
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport;
-        }
-        throw new Error("No report found.");
-    }
-    set mostRecentReport(value) {
-        if (!value) {
-            throw new Error("Please pass in a valid value!");
-        }
-        this.addReport(value);
-    }
-    constructor(id, reports) {
-        super(id, "Accounting");
-        this.reports = reports;
-        this.lastReport = reports[0];
-    }
-    // Overriding a method
-    addEmployee(employee) {
-        if (employee === "Max") {
-            return;
-        }
-        this.employees.push(employee);
-    }
-    addReport(text) {
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-    printReports() {
-        console.log(this.reports);
-    }
-}
-const accounting = new AccountingDepartment("2", []);
-accounting.addReport("Something went wrong...");
-accounting.printReports();
-console.log(accounting);
-accounting.mostRecentReport = "Year end report";
-console.log(accounting.mostRecentReport);
+const employee1 = Department.createEmployee("Max");
+console.log(employee1);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ class Department {
 
   constructor(private readonly id: string, public name: string) {}
 
+  static createEmployee(name: string) {
+    return { name: name };
+  }
+
   describe(this: Department) {
     console.log("Department: " + this.name + " (" + this.id + ")");
   }
@@ -19,53 +23,5 @@ class Department {
   }
 }
 
-// Inheritance: This class automatically gets all the properties and methods of the Department class
-// and we can add more properties and methods to extend it
-class AccountingDepartment extends Department {
-  private lastReport: string;
-
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport;
-    }
-    throw new Error("No report found.");
-  }
-
-  set mostRecentReport(value: string) {
-    if (!value) {
-      throw new Error("Please pass in a valid value!");
-    }
-    this.addReport(value);
-  }
-
-  constructor(id: string, private reports: string[]) {
-    super(id, "Accounting");
-    this.lastReport = reports[0];
-  }
-
-  // Overriding a method
-  addEmployee(employee: string): void {
-    if (employee === "Max") {
-      return;
-    }
-    this.employees.push(employee);
-  }
-
-  addReport(text: string) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
-
-  printReports() {
-    console.log(this.reports);
-  }
-}
-
-const accounting = new AccountingDepartment("2", []);
-
-accounting.addReport("Something went wrong...");
-accounting.printReports();
-console.log(accounting);
-
-accounting.mostRecentReport = "Year end report";
-console.log(accounting.mostRecentReport);
+const employee1 = Department.createEmployee("Max");
+console.log(employee1);
