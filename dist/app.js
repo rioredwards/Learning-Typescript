3e6b1ac0f2948730057a99e7a1fedc6a99d96509
Rio Edwards <rioredwards@gmail.com>
2023-03-10 18:00:15 -0800

    Type Guards

--- dist/app.js
+++ dist/app.js
@@ -5,4 +5,50 @@ const e1 = {
     privileges: ["create-server"],
     startDate: new Date(),
 };
+/* Type guards */
+// Type guards are used to check the type of a variable.
+// Three types of type guards:
+// typeof: useful for primitives
+// in: useful for objects
+// instanceof: useful for objects
+// NOTE typeof is useful for primitives, but not for objects.
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+// in is useful for objects.
+function printEmployeeInformation(emp) {
+    console.log("Name: " + emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("Start Date: " + emp.startDate);
+    }
+}
+printEmployeeInformation({ name: "Max", startDate: new Date() });
+class Car {
+    drive() {
+        console.log("Driving...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving a truck...");
+    }
+    loadCargo(amount) {
+        console.log("Loading cargo..." + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+// instanceof is useful for objects .
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -29,3 +29,63 @@ type Numeric = number | boolean;
 // The intersection operator can also be used to combine union types.
 // This type will get the common properties of both types... ie... number
 type Universal = Combinable & Numeric;
+
+/* Type guards */
+
+// Type guards are used to check the type of a variable.
+// Three types of type guards:
+// typeof: useful for primitives
+// in: useful for objects
+// instanceof: useful for objects
+
+// NOTE typeof is useful for primitives, but not for objects.
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type unknownEmployee = Employee | Admin;
+
+// in is useful for objects.
+function printEmployeeInformation(emp: unknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation({ name: "Max", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+// instanceof is useful for objects .
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
