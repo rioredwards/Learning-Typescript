c99901de481461a5778e809e1699b1973a6f512e
Rio Edwards <rioredwards@gmail.com>
2023-03-11 14:34:35 -0800

    Constraints on generics

--- src/app.ts
+++ src/app.ts
@@ -31,9 +31,32 @@ promise.then((data) => {
 // This is a function using a generic type
 // This is more specific than setting the types of the parameters to any or object
 // The return type is the intersection of the two objects
-function merge<T, U>(objA: T, objB: U) {
+// We added constraints to the parameter types... they must be objects
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return { ...objA, ...objB };
 }
 
 // Typescript is able to infer the type of the parameters and therefore the return type
 const mergedObj = merge({ name: "Rio" }, { age: 25 });
+
+// This interface is used to constrain the type of the element passed into countAndDescribe
+interface Lengthy {
+  length: number;
+}
+
+// This function uses a generic type
+// The parameter type is constrained by the Lengthy interface
+// This asserts that any parameter passed into this function must have a length property
+// We use generics to say "I don't care what the type is, but it must have a length property"
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
+console.log(countAndDescribe(["Sports", "Cooking"]));
